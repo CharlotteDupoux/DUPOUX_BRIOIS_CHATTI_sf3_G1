b0b7c9edf14cf84dc6ad694d608ac0efa5a750d9
Baptiste Briois <baptiste.briois@hotmail.fr>
2016-04-01 08:38:54 +0200

    Antispam

--- src/AppBundle/Antispam/Antispam.php
+++ src/AppBundle/Antispam/Antispam.php
@@ -4,6 +4,13 @@ namespace AppBundle\Antispam;
 
 class Antispam
 {
+    private $antispamLength;
+
+    public function __construct($antispamLength)
+    {
+        $this->antispamLength = $antispamLength;
+    }
+
     public function isSpam($text)
     {
         return strlen($text) < 50;
